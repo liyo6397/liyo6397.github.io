9500905156c1dacd8dc88b02057764982c765744
Li-Yin Young <liyin.young@colorado.edu>
2026-07-21 10:04:18 -0600

    Added the CI/CD pipeline and updated the config file for deploy the project to public

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  // 1. Your custom domain or default GitHub Pages domain
+  site: 'https://liyo6397.github.io',
+
+  // 2. The repository name as the base path (omit or use '/' if deploying to <username>.github.io)
+  base: '/',
 });
